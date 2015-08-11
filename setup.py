63e101233a069c4d66bcb578b4722e38b497e188
daniel anggrianto <danggrianto@50onred.com>
2015-08-11 12:55:34 -0400

    blns: add one function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='blns',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/danggrianto/big-list-of-naughty-strings',
     license='MIT',
     author='Daniel Anggrianto',
