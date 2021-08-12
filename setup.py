9da38c85b91a91a160aa9f1b9ee1c7a7a6927e1d
Spellchaser <klei22@bloomberg.net>
2021-08-12 15:04:53 -0400

    include data files
    needs to move __init__.py to top level of source directory

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author_email='daniel@anggrianto.com',
     description='Big List of Naughty String. Forked from https://github.com/minimaxir/big-list-of-naughty-strings',
     keywords='Big List of Naughty String',
-    packages=['blns.naughtystrings'],
+    packages=['blns'],
+    include_package_data=True,
     platforms='any',
 )
